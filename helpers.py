8cdaf20720bcbe7c8320d91acce514f7b1e9f1ec
Кирилл Гольцер <kirill.holzer@yandex.ru>
2023-07-10 23:05:05 +0300

    REFACTORED: test_registration.py assert and registration(browser, name, email, password) + ADDED: registration() to helpers.py

--- helpers.py
+++ helpers.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from locators import TestLocators
-from data import TestData
+from data import TestData, TestUrls
 
 def login(browser):
     browser.find_element(*TestLocators.EMAIL_INPUT_LOGIN).send_keys(TestData.valid_email_for_login)
@@ -9,3 +9,10 @@ def login(browser):
     browser.find_element(*TestLocators.LOGIN_BUTTON_LOGIN).click()
     WebDriverWait(browser, 3).until(
         expected_conditions.visibility_of_element_located(TestLocators.MAKE_ORDER_BUTTON))
+
+def registration(browser, name, email, password):
+    browser.get(TestUrls.register_url)
+    browser.find_element(*TestLocators.NAME_INPUT_REGISTRATION).send_keys(name)
+    browser.find_element(*TestLocators.EMAIL_INPUT_REGISTRATION).send_keys(email)
+    browser.find_element(*TestLocators.PASSWORD_INPUT_REGISTRATION).send_keys(password)
+    browser.find_element(*TestLocators.REGISTRATION_BUTTON).click()
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,7 +1,8 @@
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from locators import TestLocators
-from data import TestUrls, TestData
+from data import TestData
+from helpers import registration
 
 
 
@@ -9,34 +10,25 @@ class TestRegistration:
 
     def test_registration_success_by_password_six_symbols(self, browser):
         email, name = TestData.email()
-        browser.get(TestUrls.register_url)
-        browser.find_element(*TestLocators.NAME_INPUT_REGISTRATION).send_keys(name)
-        browser.find_element(*TestLocators.EMAIL_INPUT_REGISTRATION).send_keys(email)
-        browser.find_element(*TestLocators.PASSWORD_INPUT_REGISTRATION).send_keys(TestData.generate_correct_password_six_symbols())
-        browser.find_element(*TestLocators.REGISTRATION_BUTTON).click()
-        WebDriverWait(browser, 10).until(expected_conditions.presence_of_element_located(TestLocators.LOGIN_BUTTON_LOGIN))
-        assert browser.current_url != TestUrls.register_url
+        password = TestData.generate_correct_password_six_symbols()
+        registration(browser, name, email,password)
+        WebDriverWait(browser, 10).until(
+            expected_conditions.presence_of_element_located(TestLocators.LOGIN_BUTTON_LOGIN))
+        assert browser.find_element(*TestLocators.HEADING_ON_LOG_IN_PAGE).text == "Вход"
 
     def test_registration_success_by_password_more_then_six_symbols(self, browser):
         email, name = TestData.email()
-        browser.get(TestUrls.register_url)
-        browser.find_element(*TestLocators.NAME_INPUT_REGISTRATION).send_keys(name)
-        browser.find_element(*TestLocators.EMAIL_INPUT_REGISTRATION).send_keys(email)
-        browser.find_element(*TestLocators.PASSWORD_INPUT_REGISTRATION).send_keys(TestData.generate_correct_password_more_then_six_symbols())
-        browser.find_element(*TestLocators.REGISTRATION_BUTTON).click()
-        WebDriverWait(browser, 10).until(expected_conditions.presence_of_element_located(TestLocators.LOGIN_BUTTON_LOGIN))
-        assert browser.current_url != TestUrls.register_url
+        password = TestData.generate_correct_password_more_then_six_symbols()
+        registration(browser, name, email, password)
+        WebDriverWait(browser, 10).until(
+            expected_conditions.presence_of_element_located(TestLocators.LOGIN_BUTTON_LOGIN))
+        assert browser.find_element(*TestLocators.HEADING_ON_LOG_IN_PAGE).text == "Вход"
 
     def test_registration_failed_error(self, browser):
         email, name = TestData.email()
-        browser.get(TestUrls.register_url)
-        browser.find_element(*TestLocators.NAME_INPUT_REGISTRATION).send_keys(name)
-        browser.find_element(*TestLocators.EMAIL_INPUT_REGISTRATION).send_keys(email)
-        browser.find_element(*TestLocators.PASSWORD_INPUT_REGISTRATION).send_keys(TestData.generate_uncorrect_password_less_then_six_symbols())
-        browser.find_element(*TestLocators.REGISTRATION_BUTTON).click()
+        password = TestData.generate_uncorrect_password_less_then_six_symbols()
+        registration(browser, name, email, password)
         WebDriverWait(browser, 10).until(
             expected_conditions.presence_of_element_located(TestLocators.PASSWORD_INPUT_ERROR_REGISTRATION))
-        browser.find_element(*TestLocators.PASSWORD_INPUT_ERROR_REGISTRATION)
         expected_text = "Некорректный пароль"
         assert browser.find_element(*TestLocators.PASSWORD_INPUT_ERROR_REGISTRATION).text == expected_text
-
